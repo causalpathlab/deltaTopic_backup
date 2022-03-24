c822888a36d92a800104647ed12523b8903b0d53
thisisyichenzhang <zhangyichen93@gmail.com>
2022-03-23 22:26:51 -0700

    Train DeltaETM

--- Train_DeltaETM_PDAC.py
+++ Train_DeltaETM_PDAC.py
@@ -7,31 +7,33 @@ from scvi.data import setup_anndata
 import wandb
 import argparse
 from scipy.sparse import csr_matrix
+import scanpy
 #%%
 wandb.login()
-#wandb.init(entity="thisisyichen")
+wandb.init(entity="thisisyichen")
 # Input parser
-#parser = argparse.ArgumentParser(description='Parameters for NN')
-#parser.add_argument('--EPOCHS', type=int, help='EPOCHS', default=500)
-#parser.add_argument('--learning_rate', type=float, help='learning_rate', default=1e-3)
-#parser.add_argument('--nLV', type=int, help='User specified nLV', default=10)
+parser = argparse.ArgumentParser(description='Parameters for NN')
+parser.add_argument('--EPOCHS', type=int, help='EPOCHS', default=500)
+#parser.add_argument('--lr', type=float, help='learning_rate', default=1e-3)
+#parser.add_argument('--CUDA', type=int, help='which GPU to use', default=0)
+parser.add_argument('--nLV', type=int, help='User specified nLV', default=4)
+parser.add_argument('--bs', type=int, help='Batch size', default=256)
 #parser.add_argument('--concat_method', type=str, help='Pathway type', default='cat')
-#args = parser.parse_args()
+args = parser.parse_args()
 # pass args to wand.config
-#wandb.config.update(args)
+wandb.config.update(args)
 #%%
 DataDIR = os.path.join(os.path.expanduser('~'), "projects/data")
-adata = scvi.data.read_h5ad(os.path.join(DataDIR,'CRA001160/final_CRA001160_spliced.h5ad'))
+adata_spliced = scvi.data.read_h5ad(os.path.join(DataDIR,'CRA001160/final_CRA001160_spliced_allgenes.h5ad'))
+adata_unspliced = scvi.data.read_h5ad(os.path.join(DataDIR,'CRA001160/final_CRA001160_unspliced_allgenes.h5ad'))
 # for speed-up of training
-adata.layers["counts"] = csr_matrix(adata.X).copy()
+adata_spliced.layers["counts"] = csr_matrix(adata_spliced.X).copy()
+adata_unspliced.layers["counts"] = csr_matrix(adata_unspliced.X).copy()
 # save raw adata 
-adata.raw = adata
-# get PDAC and normal samples
-adata_PDAC = adata[adata.obs.tumor_type == "Tumor",:]
-adata_normal = adata[adata.obs.tumor_type == "Normal",:]
-
+adata_spliced.raw = adata_spliced
+adata_unspliced.raw = adata_unspliced
 #%%
-pathways = anndata.read_h5ad(os.path.join(DataDIR,'pathways/Hallmark.h5ad'))
+'''pathways = anndata.read_h5ad(os.path.join(DataDIR,'pathways/Hallmark.h5ad'))
 # concat relevant pathways
 
 #ad_tst = anndata.read_h5ad("data/pathways/chemgenPathways.h5ad")
@@ -40,45 +42,52 @@ pathways = anndata.read_h5ad(os.path.join(DataDIR,'pathways/Hallmark.h5ad'))
 #ad_tst4 = anndata.read_h5ad("data/pathways/canonicalPathways.h5ad")
 #ad_tst5 = anndata.read_h5ad("data/pathways/oncogenicPathways.h5ad")
 #ad_tst6 = anndata.read_h5ad("data/pathways/xCell.h5ad")
-#pathways = anndata.concat([ad_tst, ad_tst2,ad_tst3, ad_tst4,ad_tst5, ad_tst6], join="outer", fill_value = 0)
+#pathways = anndata.concat([ad_tst, ad_tst2,ad_tst3, ad_tst4,ad_tst5, ad_tst6], join="outer", fill_value = 0)'''
+#%%
+setup_anndata(adata_spliced, layer="counts", batch_key="sample_id")
+setup_anndata(adata_unspliced, layer="counts", batch_key="sample_id")
 #%% 
 # set up anndata
 # get the common genes in pathway and adata
-df_cm = adata_PDAC.var.join(pathways.var, how = 'inner')
-# register adata_source1
-adata_PDAC_input = adata_PDAC[:,df_cm.index.to_list()].copy()
-setup_anndata(adata_PDAC_input, layer="counts", batch_key="sample_id", labels_key="tumor_type")
+'''df_cm = adata_spliced.var.join(pathways.var, how = 'inner')
+# register adata_spliced
+adata_spliced_input = adata_spliced[:,df_cm.index.to_list()].copy()
+setup_anndata(adata_spliced_input, layer="counts", batch_key="sample_id")
 
-# register adata_source2
-adata_normal_input = adata_normal[:,df_cm.index.to_list()].copy()
-setup_anndata(adata_normal_input, layer="counts", batch_key="sample_id", labels_key="tumor_type")
+# register adata_unspliced
+adata_unspliced_input = adata_unspliced[:,df_cm.index.to_list()].copy()
+scanpy.pp.filter_cells(adata_unspliced_input, min_counts=10)
+setup_anndata(adata_unspliced_input, layer="counts", batch_key="sample_id")
 # register pathway 
 pathways.var_names_make_unique()
 pathways_input = pathways[:,df_cm.index.to_list()].copy()
-setup_anndata(pathways_input)
+setup_anndata(pathways_input)'''
 #%%
 #create our model
 from DeltaETM_model import DeltaETM
-model = DeltaETM(adata_PDAC_input, adata_normal_input, adata_pathway = pathways_input)
+model = DeltaETM(adata_spliced, adata_unspliced, n_latent = args.nLV)
+#model = DeltaETM(adata_spliced_input, adata_unspliced_input, adata_pathway = pathways_input)
 #model = scCLR(adata_tumor_input, adata_metastatic_input, mask = torch.bernoulli(torch.empty(100, 16445).uniform_(0, 1)))
 #%%
 from pytorch_lightning.loggers import WandbLogger
 # this has to be passed, otherwise pytroch lighting logging won't be passed to wandb
 wandb_logger = WandbLogger(project = 'DeltaETM')
+#model_kwargs = {"lr": args.lr}
 model.train(
-    5, 
+    args.EPOCHS, 
     check_val_every_n_epoch=5,
-    batch_size=256,
+    batch_size=args.bs,
     logger = wandb_logger,
+#    model_kwargs,
     )
 #%%
-
-'''model.save("models/scCLR_masked_decoder_pancreas_final_QC_no_softmax_new", overwrite=True, save_anndata=True)
-
+savefile_name = f"models/DeltaETM_allgenes_ep{args.EPOCHS}_nlv{args.nLV}_bs{args.bs}"
+model.save(savefile_name, overwrite=True, save_anndata=True)
+print(f"Model saved to {savefile_name}")
 ########
 
 
-import scanpy as sc
+'''import scanpy as sc
 import matplotlib.pyplot as plt
 
 # concatenate adata to get all cells
