0067b9312d1eb15bc8a1a739cccc576ee6cc06b7
thisisyichenzhang <zhangyichen93@gmail.com>
2022-03-29 00:30:49 -0700

    update

--- run_analysis.sh
+++ run_analysis.sh
@@ -6,7 +6,7 @@ for path in models/TotalDeltaETM_allgenes_ep1000_nlv4_bs512_combinebyadd_lr0.01_
 echo $path
 python get_latent.py --SavePath $path
 Rscript analysis/plot_latent.R --SavePath $path
-Rscript analysis/plot_weights.R --SavePath$path
+Rscript analysis/plot_weights.R --SavePath $path
 done
 
   
