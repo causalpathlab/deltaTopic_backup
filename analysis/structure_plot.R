c4765068bf555aa396e8e5620465e47d9a8e0936
thisisyichenzhang <zhangyichen93@gmail.com>
2022-04-07 18:15:00 -0700

    include structure plot to the analysis pipeline

--- analysis/structure_plot.R
+++ analysis/structure_plot.R
@@ -1,17 +1,18 @@
-library(Matrix)
 library(data.table)
 library(ggplot2)
 library(argparse)
 library(magrittr)
 library(tidyr)
-#library(ggpubr)
+parser <- ArgumentParser()
 
-
-SaveFolderPath <- "models/TotalDeltaETM_allgenes_ep1000_nlv32_bs512_combinebyadd_lr0.01_train_size1"
+parser$add_argument("--SavePath",help = "relative path to save folder")
+args <- parser$parse_args()
+#SaveFolderPath <- "models/TotalDeltaETM_allgenes_ep1000_nlv32_bs512_combinebyadd_lr0.01_train_size1"
+SaveFolderPath = args$SavePath
 DT <- fread(paste(SaveFolderPath, "topics.csv", sep = "/")) %>% as.data.frame()
 
 rownames(DT) <- DT$V1
-DT <- DT[,-1] 
+DT <- DT[, -1] 
 topics_wide <- DT
 topics_wide$max_topic <- colnames(DT)[apply(DT, 1, which.max)]
 topics_wide$max_topic_prob <- apply(DT, 1, max)
@@ -27,9 +28,8 @@ table(topics_wide$max_topic)
 
 dt <- data.table(topics_wide, key="max_topic")
 dt$cell <- as.factor(rownames(topics_wide))
-K = 100
+K <- 1000
 dt[, .SD[max_topic_prob %in% head(sort(unique(max_topic_prob)), K)], by=max_topic] %>% as.data.frame() -> topics_wide_topK
-
 gather(topics_wide_topK, topic, probabilities, colnames(topics_wide_topK)[2:(ncol(topics_wide_topK)-2)], factor_key=TRUE) -> topics_long
 
 p <- ggplot(topics_long, aes(x = cell, y = probabilities, fill=topic)) +
@@ -49,8 +49,6 @@ p <- ggplot(topics_long, aes(x = cell, y = probabilities, fill=topic)) +
     panel.grid = element_blank(),
     panel.background = element_rect(fill='transparent'),
     plot.background = element_rect(fill='transparent', color=NA))+
-  guides(fill = guide_legend(nrow = 2))
+  guides(fill = guide_legend(nrow = 1))
 #p
-
-ggsave(paste(SaveFolderPath,"struct_plot.pdf",sep = "/"))
-
+ggsave(paste(SaveFolderPath, "struct_plot.pdf", sep = "/"))
--- run_analysis.sh
+++ run_analysis.sh
@@ -2,9 +2,12 @@
 
 #models/TotalDeltaETM_allgenes_ep1000_nlv4_bs512_combinebyadd_lr0.01_train_size1
 #models/TotalDeltaETM_allgenes_ep1000_nlv8_bs512_combinebyadd_lr0.01_train_size1
+#models/TotalDeltaETM_allgenes_ep2000_nlv16_bs512_combinebyadd_lr0.01_train_size1
+#models/TotalDeltaETM_allgenes_ep1000_nlv32_bs512_combinebyadd_lr0.01_train_size1
 for path in models/TotalDeltaETM_allgenes_ep1000_nlv4_bs512_combinebyadd_lr0.01_train_size1 models/TotalDeltaETM_allgenes_ep1000_nlv8_bs512_combinebyadd_lr0.01_train_size1;do 
     echo $path
     python get_latent.py --SavePath $path
+    Rscript analysis/structure_plot.R --SavePath $path
     Rscript analysis/plot_weights.R --SavePath $path
     Rscript analysis/phase_plot.R --SavePath $path
 done
